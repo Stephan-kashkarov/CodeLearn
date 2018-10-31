5d8d8aed42a14f8ae3ddbeb2ed143776b60272e6
Stephan-Kashkarov <KashkarovWork@gmail.com>
2018-11-01 01:02:16 +1100

    finished serverside marking

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import routes
+from app import routes
--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,2 +1,35 @@
 from app import app
 from flask import request
+import json
+import traceback
+
+answers = {
+	1: "Hello, World!"
+}
+
+def _print(s):
+	return s
+
+@app.route('/mark', methods=['POST'])
+def mark():
+	if request:
+		data = request.json
+		answer = answers[data['question']]
+		error = False
+		code = data['code']
+		print(code)
+		try:
+			code = eval(code)
+			print(">", code)
+		except:
+			error = True
+			code = traceback.format_exc()
+
+		if code == answer:
+			return 'true'
+		elif error:
+			return code
+		else:
+			return str("{} is not the same as {}".format(code, answer))
+
+	return 'false'
