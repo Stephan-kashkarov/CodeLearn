06e4a152f8364e7e16d44d37cbf7d04a8069c13b
Stephan-Kashkarov <KashkarovWork@gmail.com>
2018-11-01 01:22:31 +1100

    Almost finished marking on frontend, Cant get to flask server for some reason

--- Frontend/js/editor.js
+++ Frontend/js/editor.js
@@ -6,4 +6,34 @@ $(function(){
 		 	setTimeout(hljs.highlightBlock(block), 2000)
 		})
 	})
+
+	$('button:contains("Mark!")').on('click', function(){
+		console.log('click');
+		
+		var question = $('.active').text()
+		var questionNumber = question.substr(question.length - 1)
+		var code = $('#code').text()
+		var ans = $.ajax({
+			url: "127.0.0.1:5000/mark",
+			method: "POST",
+			async: false,
+			data: JSON.stringify({
+				'question': questionNumber,
+				'code': code
+			}),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function (data) {
+				console.log("Ajax succsess: " + data)
+			},
+			failure: function (errMsg) {
+				console.log("Ajax error: " + errMsg)
+			}
+		})
+		if (ans == 'true'){
+			console.log('correct answer');
+		} else {
+			console.log(ans);
+		}
+	})
 })
