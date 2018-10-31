df2dfdb6d077157b7066b1390722617b5ff34b48
Stephan-Kashkarov <KashkarovWork@gmail.com>
2018-11-01 00:23:44 +1100

    added highlighting with highlight.js will make my own later

--- Frontend/js/editor.js
+++ Frontend/js/editor.js
@@ -1,3 +1,9 @@
+hljs.initHighlightingOnLoad()
 $(function(){
-	
+	// Syntax highlighting
+	$('button:contains("Highlight!")').on('click', function (e) {
+		$('pre code').each(function (i, block) {
+		 	setTimeout(hljs.highlightBlock(block), 2000)
+		})
+	})
 })
