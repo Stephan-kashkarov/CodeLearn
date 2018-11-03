83848f3b7b7dae94696846322664c51404e3cc0b
Stephan-Kashkarov <KashkarovWork@gmail.com>
2018-11-03 16:37:15 +1100

    removed duplicate functions

--- Frontend/js/editor.js
+++ Frontend/js/editor.js
@@ -76,42 +76,4 @@ $(function(){
 		console.log(questions);
 		window.localStorage.setItem("questions", JSON.stringify(questions))
 	})
-
-	$('button:contains("Next!")').on('click', function () {
-		if (!($(this).hasClass('disabled'))){
-			var question = $('.active').text()
-			var questionNumber = question.substr(question.length - 1)
-			var newQuestion = question.substr(0, question.length - 1) + (eval(questionNumber) + 1)
-			var nextQuestion = question.substr(0, question.length - 1) + (eval(questionNumber) + 2)
-			if (newQuestion !== "Question7") {
-				$('button:contains("Prev!")').removeClass('disabled')
-				$('.nav-item:contains("' + question + '")').removeClass('active')
-				$('.nav-item:contains("' + newQuestion + '")').addClass('active')
-				if ($('.nav-item:contains("'+ nextQuestion + '")').hasClass('disabled')) {
-					$(this).addClass('disabled')
-				}
-			} else {
-				$(this).addClass('disabled')
-			}
-		}
-	})
-		
-	$('button:contains("Prev!")').on('click', function () {
-		if (!($(this).hasClass('disabled'))) {
-			var question = $('.active').text()
-			var questionNumber = question.substr(question.length - 1)
-			var newQuestion = question.substr(0, question.length - 1) + (eval(questionNumber) - 1)
-			var nextQuestion = question.substr(0, question.length - 1) + (eval(questionNumber) - 2)
-			if (newQuestion !== "Question0"){
-				$('button:contains("Next!")').removeClass('disabled')
-				$('.nav-item:contains("' + question + '")').removeClass('active')
-				$('.nav-item:contains("' + newQuestion + '")').addClass('active')
-				if ($('.nav-item:contains("' + nextQuestion + '")').hasClass('disabled')) {
-					$(this).addClass('disabled')
-				}
-			} else {
-				$(this).addClass('disabled')
-			}
-		}
-	})
 })
